7893f6dc4fc46b550ebc52432e8bd08b4c65cc7b
Ilya Kozak <kozak@tut.by>
2020-09-04 15:51:49 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let bracketsSequence = str;
+    if (bracketsSequence.length % 2 !== 0) return false;
+
+    const parentheses = "()[]{}||";
+    let regExpStr = "";
+    for (let i = 0; i < bracketsConfig.length; i++) {
+        if (parentheses.includes(bracketsConfig[i][0])) {
+            regExpStr +=
+                "(\\" +
+                bracketsConfig[i][0] +
+                "\\" +
+                bracketsConfig[i][1] +
+                ")";
+        } else {
+            regExpStr +=
+                "(" + bracketsConfig[i][0] + bracketsConfig[i][1] + ")";
+        }
+        if (i < bracketsConfig.length - 1) regExpStr += "|";
+    }
+
+    const re = new RegExp(regExpStr);
+    while (re.test(bracketsSequence))
+        bracketsSequence = bracketsSequence.replace(re, "");
+    return !bracketsSequence;
+};
